adc70720b89866a253036a673e093fd6ca2ab002
Monica Wolfe <mcwolfe2011@gmail.com>
2023-03-16 21:02:37 -0700

    Got api working, for some call back

---
+++ index2.js
@@ -0,0 +1,22 @@
+// index2.js
+
+const { fetchMyIP, fetchCoordsByIP } = require('./iss_promised');
+
+fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then(body => console.log(body));
+
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+// see index.js for printPassTimes
+// copy it from there, or better yet, moduralize and require it in both files
+
+// Call
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
